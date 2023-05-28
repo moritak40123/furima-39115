e6c4b0a8aa1701f159dcaa4570d23fa9ba1ea1a4
moritak40123 <moritak40123@gmail.com>
2023-05-28 09:15:37 +0900

    商品テーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
+    @items = Item.includes(:user)
   end
 
-  # def new
-  # end
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :price, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :profile, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :ship_date_id, presence: true
+  belongs_to :user
+  has_one_attached :image
 end
