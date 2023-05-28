156158534a352317a4bbfa94247db27dc11f4260
moritak40123 <moritak40123@gmail.com>
2023-05-28 18:15:47 +0900

    商品保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def index
     @items = Item.includes(:user)
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
@@ -18,13 +18,13 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
+  # def show
+  # end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :price, :delivery_charge_id, :profile, :category_id, :condition_id, :prefecture_id, :ship_date_id, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :ship_date
+
   validates :item_name, presence: true
-  validates :price, presence: true
-  validates :delivery_charge_id, presence: true
+  with_options presence: true, format: { with: /\A[1-9][0-9]{2,6}\z/, message: 'には半角数値で入力してください' } do
+    validates :price
+  end
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :profile, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :ship_date_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image, presence: true
   belongs_to :user
   has_one_attached :image
+
 end
