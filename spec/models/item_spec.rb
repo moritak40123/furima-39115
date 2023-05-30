01380a92bb02d281479df24d67384af5aafbfeba
moritak40123 <moritak40123@gmail.com>
2023-05-30 22:08:59 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,87 +7,88 @@ RSpec.describe Item, type: :model do
 
   describe '商品の保存' do
     context '商品が出品できる場合' do
-      it '全項目が存在していれば出品できる' do
+      it 'item_name、price、delivery_charge_id、profile、category_id、
+      condition_id、prefecture_id、ship_date_id、userが存在していれば出品できる' do
         expect(@item).to be_valid
       end
     end
     context '商品が出品できない場合' do
-      it '商品画像が空では投稿できない' do
+      it 'imageが空では投稿できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it '商品名が空では投稿できない' do
+      it 'item_nameが空では投稿できない' do
         @item.item_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
-      it '商品の説明が空では投稿できない' do
+      it 'profileが空では投稿できない' do
         @item.profile = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Profile can't be blank")
       end
-      it 'カテゴリーが空では投稿できない' do
+      it 'category_idが空では投稿できない' do
         @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it 'カテゴリーが---では投稿できない' do
+      it 'category_idが---では投稿できない' do
         @item.category_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it '商品の状態が空では投稿できない' do
+      it 'condition_idが空では投稿できない' do
         @item.condition_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      it '商品の状態が---では投稿できない' do
+      it 'condition_idが---では投稿できない' do
         @item.condition_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      it '配送料の負担が空では投稿できない' do
+      it 'delivery_charge_idが空では投稿できない' do
         @item.delivery_charge_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
-      it '配送料の負担が---では投稿できない' do
+      it 'delivery_charge_idが---では投稿できない' do
         @item.delivery_charge_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
-      it '発送元の地域が空では投稿できない' do
+      it 'prefecture_idが空では投稿できない' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it '発送元の地域が---では投稿できない' do
+      it 'prefecture_idが---では投稿できない' do
         @item.prefecture_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it '発送までの日数が空では投稿できない' do
+      it 'ship_date_idが空では投稿できない' do
         @item.ship_date_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship date can't be blank")
       end
-      it '発送までの日数が---では投稿できない' do
+      it 'ship_date_idが---では投稿できない' do
         @item.ship_date_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship date can't be blank")
       end
-      it '価格が空では投稿できない' do
+      it 'priceが空では投稿できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格が300から9,999,999の半角数字でないと投稿できない' do
+      it 'priceが300から9,999,999の半角数字でないと投稿できない' do
         @item.price = 10
         @item.valid?
         expect(@item.errors.full_messages).to include("Price には300から9,999,999の半角数値で入力してください")
       end
-      it 'ユーザーが紐付いていなければ投稿できない' do
+      it 'userが紐付いていなければ投稿できない' do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
