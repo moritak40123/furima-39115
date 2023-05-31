4c2ab84cbd04dc69d3b383fb65261563bfae92eb
moritak40123 <moritak40123@gmail.com>
2023-05-31 21:25:42 +0900

    Rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   belongs_to :ship_date
 
   validates :item_name, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'には300から9,999,999の半角数値で入力してください' }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'には300から9,999,999の半角数値で入力してください' }
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :profile, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
