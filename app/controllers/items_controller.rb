d0c271cfc66d62be6e8d2708e91782eb569cc874
moritak40123 <moritak40123@gmail.com>
2023-06-04 17:54:20 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,8 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
-      redirect_to action: :index
-    end
-  end
+    return if current_user.id == @item.user_id
 
+    redirect_to action: :index
+  end
 end
