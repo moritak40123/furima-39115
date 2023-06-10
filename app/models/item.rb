61acf815df235f62e4801599f6bac2aae7633347
moritak40123 <moritak40123@gmail.com>
2023-06-10 20:30:11 +0900

    配送先Formオブジェクトパターン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,5 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :delivery_charge
-  belongs_to :prefecture
-  belongs_to :ship_date
-
   validates :item_name, presence: true
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'には300から9,999,999の半角数値で入力してください' }
@@ -16,6 +10,13 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :ship_date_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :image, presence: true
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :ship_date
   belongs_to :user
   has_one_attached :image
+  has_one :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  belongs_to :prefecture
   has_one :address
 end
