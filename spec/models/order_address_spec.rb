2452bc4875663721de830e5c633fdd6af619f296
moritak40123 <moritak40123@gmail.com>
2023-06-11 21:38:26 +0900

    テストコード修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe OrderAddress, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
-      sleep 0.1
     end
 
     context '内容に問題ない場合' do
